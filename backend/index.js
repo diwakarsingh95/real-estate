3d35a907a3af4d47e9b6dc3e99c869a3e1e540ae
Diwakar <diwakar.singh313@gmail.com>
2023-11-29 04:51:40 +0530

    setup mongodb and mongoose

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,18 @@
+import "dotenv/config";
 import express from "express";
+import mongoose from "mongoose";
 
 const port = process.env.PORT || 3000;
 const app = express();
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log("Connected to MongoDB!");
+  })
+  .catch((err) => {
+    console.error(err);
+  });
 
 app.listen(3000, () => {
   console.log(`Server is running on port ${port}!`);
