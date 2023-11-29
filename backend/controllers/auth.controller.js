e9fd0a21a087eed1e26bdc9a361330389a04d7eb
Diwakar <diwakar.singh313@gmail.com>
2023-11-29 07:21:07 +0530

    added sign feature

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,5 +1,7 @@
 import bcryptjs from "bcryptjs";
+import jwt from "jsonwebtoken";
 import User from "../models/user.model.js";
+import { errorHandler } from "../utils/error.js";
 
 export const signUp = async (req, res, next) => {
   const { username, email, password } = req.body;
@@ -10,6 +12,26 @@ export const signUp = async (req, res, next) => {
   try {
     await newUser.save();
     res.status(201).json("User created successfully!");
+  } catch (error) {
+    console.error(error);
+    next(error);
+  }
+};
+
+export const signIn = async (req, res, next) => {
+  const { email, password } = req.body;
+
+  try {
+    const validUser = await User.findOne({ email });
+    if (!validUser) return next(errorHandler(404, "User not found!"));
+    const validPassword = bcryptjs.compareSync(password, validUser.password);
+    if (!validPassword) return next(errorHandler(401, "Wrong credentials!"));
+    const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
+    const { password: pass, ...userInfo } = validUser._doc;
+    res
+      .cookie("access_token", token, { httpOnly: true })
+      .status(200)
+      .json(userInfo);
   } catch (error) {
     console.error(error);
     next(error);
