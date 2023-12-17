751bf34ccdec4a490cef9c67ae3302c25435e9b3
Diwakar <diwakar.singh313@gmail.com>
2023-12-17 16:05:14 +0530

    added get listings api

--- backend/controllers/listing.controller.js
+++ backend/controllers/listing.controller.js
@@ -1,5 +1,14 @@
 import Listing from "../models/listing.model.js";
 
+export const getListings = async (req, res, next) => {
+  try {
+    const listings = await Listing.find({ userRef: req.user.id });
+    res.status(200).json(listings);
+  } catch (error) {
+    next(error);
+  }
+};
+
 export const createListing = async (req, res, next) => {
   try {
     const listing = await Listing.create(req.body);
--- backend/routes/listing.route.js
+++ backend/routes/listing.route.js
@@ -1,9 +1,10 @@
 import express from "express";
-import { createListing } from "../controllers/listing.controller.js";
+import { createListing, getListings } from "../controllers/listing.controller.js";
 import { verifyToken } from "../utils/verifyToken.js";
 
 const router = express.Router();
 
+router.get("/", verifyToken, getListings);
 router.post("/create", verifyToken, createListing);
 
 export default router;
