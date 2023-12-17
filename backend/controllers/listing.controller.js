fd48d6632df893ed839135d2da9bdbb7d416341e
Diwakar <diwakar.singh313@gmail.com>
2023-12-17 19:09:40 +0530

    added delete listing api

--- backend/controllers/listing.controller.js
+++ backend/controllers/listing.controller.js
@@ -1,4 +1,5 @@
 import Listing from "../models/listing.model.js";
+import { errorHandler } from "../utils/errorHandler.js";
 
 export const getListings = async (req, res, next) => {
   try {
@@ -13,6 +14,23 @@ export const createListing = async (req, res, next) => {
   try {
     const listing = await Listing.create(req.body);
     res.status(201).json(listing);
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const deleteListing = async (req, res, next) => {
+  try {
+    const { listingId } = req.params;
+    const listing = await Listing.findById(listingId);
+
+    if (!listing) return next(errorHandler(404, "No listing found!"));
+
+    if (listing._id.toString() !== listingId)
+      return next(errorHandler(401, "You can only delete your own listings!"));
+
+    const deletedListingData = await Listing.findByIdAndDelete(listingId);
+    res.status(200).json(deletedListingData);
   } catch (error) {
     next(error);
   }
--- backend/routes/listing.route.js
+++ backend/routes/listing.route.js
@@ -1,10 +1,15 @@
 import express from "express";
-import { createListing, getListings } from "../controllers/listing.controller.js";
+import {
+  createListing,
+  deleteListing,
+  getListings,
+} from "../controllers/listing.controller.js";
 import { verifyToken } from "../utils/verifyToken.js";
 
 const router = express.Router();
 
 router.get("/", verifyToken, getListings);
 router.post("/create", verifyToken, createListing);
+router.delete("/delete/:listingId", verifyToken, deleteListing);
 
 export default router;
