8cffe1bcb2ed205b82cc2e00fc6e8c3369116c5a
Diwakar <diwakar.singh313@gmail.com>
2023-12-31 21:45:32 +0530

    udpated about page

--- client/src/pages/Home/Home.tsx
+++ client/src/pages/Home/Home.tsx
@@ -13,7 +13,7 @@ const Home = () => {
           place with ease
         </h1>
         <div className="text-gray-400 text-xs sm:text-sm">
-          Sahand Estate is the best place to find your next perfect place to
+          Real Estate is the best place to find your next perfect place to
           live.
           <br />
           We have a wide range of properties for you to choose from.
